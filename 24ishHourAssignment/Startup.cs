d4b37fe4e2639ba735a6ba1b111174a23aa76b1a
normabunton <te_nj25@hotmail.com>
2021-02-02 20:28:11 -0500

    initial commit

---
+++ 24ishHourAssignment/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(_24ishHourAssignment.Startup))]
+
+namespace _24ishHourAssignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
